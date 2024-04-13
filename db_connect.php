7061b96fcc9c7c646d1d34355f851ce4a6642758
Austin Tao <austin.t7897@gmail.com>
2024-04-13 14:04:54 -0700

    updating htmls, php
    - the edit page doubles as a lookup, and we can make each result a clickable to make edits
    - conjecture: the php should dynamically change process based on what function is needed
    -> maybe i could map the sql functions and call that way?

--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,7 @@
 <?php
+
+// TODO: verify username and password (hash verification?)
+
 // Establish connection to the database (TODO: deal with 'your_username' and 'your_password')
 $serverName = "bughound_server";
 $connectionOptions = array("Database" => "bughound_db", "Uid" => "your_username", "PWD" => "your_password");
@@ -8,8 +11,23 @@ if ($conn === false) {
     die("Failed to connect to the database: " . sqlsrv_errors());
 }
 
-// Define the SQL query (TODO: $sql = sql_call_string)
+// Define the SQL query (note: case "read")
 $sql = "SELECT * FROM bugs";
+
+// TODO: look this over, change as needed in relation to access level and parameters (maybe create separate functions)
+switch($function_being_used) {
+    case "create_bug":
+        $sql = "INSERT INTO bugs (bug_title, bug_description, reporter_id, bug_severity) VALUES (Some_bug_title, Some_bug_desc, A_reporter_id, Some_severity_lvl)";
+        break;
+    case "read_bugs": // this is the default sql string ^^
+        break;
+    case "update_resolution":
+        $sql = "UPDATE resolutions SET resolution_status = resolve_state WHERE bug_id = Some_bug_id AND report_id = Some_report_id";
+        break;
+    case "delete_bug":
+        $sql = "DELETE FROM bugs WHERE bug_id = Some_bug_id";
+        break;
+}
 
 // Execute the query
 $stmt = sqlsrv_query($conn, $sql);
