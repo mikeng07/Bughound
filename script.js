cbacd3f85d9fa48fd3e7468de8c650dd9fe2e6c2
Austin Tao <austin.t7897@gmail.com>
2024-04-15 21:04:13 -0700

    resetButton functionality
    - option boxes have a default value, and return to it when the reset button is pressed

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Get the button elements by ID
 var createButton = document.getElementById("createButton");
 var editButton = document.getElementById("editButton");
+var backButton = document.getElementById("backButton");
 
 // Check if createButton exists before adding event listener
 if (createButton) {
@@ -17,10 +18,40 @@ if (editButton) {
 }
 
 // Add event listener to the backButton
-var backButton = document.getElementById("backButton");
 if (backButton) {
     backButton.addEventListener("click", function() {
         window.location.href = "mainPage.html";
+    });
+}
+
+// reset all values in createPage.html
+var resetButton = document.getElementById("resetButton");
+if (resetButton) {
+    resetButton.addEventListener("click", function() {
+        console.log('resetting...');
+        $("#program").val("default");
+        $("#reportType").val("default");
+        $("#severity").val("default");
+        document.getElementById("reproducible").checked = false;
+        document.getElementById("problemSummary").value = "";
+        document.getElementById("problem").value = "";
+        document.getElementById("suggestedFix").value = "";
+        $("#reportedBy").val("default");
+        document.getElementById('dateInput').value = "";
+        document.getElementByName('attachments').value = "";
+        
+        $("#functionalArea").val("default");
+        $("#assignTo").val("default");
+        document.getElementById("comments").value = "";
+        $("#status").val("default");
+        document.getElementById('priority').selected = "0";
+        $("#resolution").val("default");
+        $("#resolutionVersion").val("default");
+        $("#resolvedBy").val("default");
+        document.getElementById('dateResolved').value = "";
+        $("#testedBy").val("default");
+        document.getElementById('dateTested').value = "";
+        document.getElementById("treatAsDeferred").checked = false;
     });
 }
 
@@ -30,6 +61,9 @@ if (searchButton) {
     searchButton.addEventListener("click", function() {
         // Your search button functionality here
 
+        // post and await reply
+
+        // default / if reply turns up with nothing
         $('#searchResults').html("No results found");
     });
 }
@@ -40,5 +74,12 @@ if (submitButton) {
         // Your search button functionality here
 
         $('#spinning').toggle();
+
+        // insert post and await reply from server
+
+        // reply complete, stop spinning
+        // $('#spinning').toggle();
+
+        // insert reply results
     });
 }
