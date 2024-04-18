146234d67ed7ea8d35bdb2e431eb78b0d88dbad1
Austin Tao <austin.t7897@gmail.com>
2024-04-17 21:30:42 -0700

    Update bughound_db.sql
    - undid procedure 'createTables'
    - the tables should be good now (big SHOULD BE)
    - table 'version_areas' connect an area type to a version of a release of a program

--- bughound_db.sql
+++ bughound_db.sql
@@ -2,83 +2,88 @@
 -- TODO: check if this is right
 -- TODO: change varchar lengths based on what the variable is
 
-create procedure createTables()
-BEGIN
-    CREATE TABLE users (
-        user_id         INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        user_name       VARCHAR(255) NOT NULL,
-        user_pass       VARCHAR(255) NOT NULL,
-        user_realname   VARCHAR(255) NOT NULL,
-        user_access     INT NOT NULL DEFAULT 1   -- note: access_lvl (1:emp, 2:manager, 3:admin)
-    );
-    CREATE TABLE bugs (
-        bug_id              INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        bug_title           VARCHAR(255) NOT NULL,
-        bug_severity        VARCHAR(255) NOT NULL,
-        bug_description     VARCHAR(255) NOT NULL,
-        bug_fix_suggestion  VARCHAR(255),
-        user_reporter_id    INT NOT NULL,
-        FOREIGN KEY (user_reporter_id) REFERENCES users(user_id),
-        bug_find_date       DATE NOT NULL
-    );
-    CREATE TABLE programs (
-        program_id          INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        program_name        VARCHAR(255) NOT NULL
-    );
-    CREATE TABLE program_releases (
-        release_id          INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        release_name        VARCHAR(255) NOT NULL,
+CREATE TABLE users (
+    user_id         INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_name       VARCHAR(255) NOT NULL,
+    user_pass       VARCHAR(255) NOT NULL,
+    user_realname   VARCHAR(255) NOT NULL,
+    user_access     INT NOT NULL DEFAULT 1   -- note: access_lvl (1:emp, 2:manager, 3:admin)
+);
 
-        program_id          INT NOT NULL,
-        FOREIGN KEY (program_id) REFERENCES programs(program_id)
-        
-    );
-    CREATE TABLE release_versions (
-        version_id          INT NOT NULL PRIMARY KEY,
-        version_name        VARCHAR(255) NOT NULL,
+CREATE TABLE bugs (
+    bug_id              INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    bug_title           VARCHAR(255) NOT NULL,
+    bug_severity        VARCHAR(255) NOT NULL,
+    bug_description     VARCHAR(255) NOT NULL,
+    bug_fix_suggestion  VARCHAR(255),
+    user_reporter_id    INT NOT NULL,
+    bug_find_date       DATE NOT NULL,
+    FOREIGN KEY (user_reporter_id) REFERENCES users(user_id)            -- connects to users:user_id
+);
 
-        release_id          INT NOT NULL,
-        FOREIGN KEY (release_id) REFERENCES program_releases (release_id)
+CREATE TABLE programs (
+    program_id          INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    program_name        VARCHAR(255) NOT NULL
+);
+CREATE TABLE program_releases (
+    release_id          INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    release_name        VARCHAR(255) NOT NULL,
+    program_id          INT NOT NULL,
+    FOREIGN KEY (program_id) REFERENCES programs(program_id)    
+);
+CREATE TABLE release_versions (
+    version_id          INT NOT NULL PRIMARY KEY,
+    version_name        VARCHAR(255) NOT NULL,
+    release_id          INT NOT NULL,
+    FOREIGN KEY (release_id) REFERENCES program_releases (release_id)
+);
+CREATE TABLE areas (
+    area_id             INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    area_title          VARCHAR(255) NOT NULL
+);
+CREATE TABLE version_areas (                                            -- bridge table b/t 'release_versions' and 'areas'
+    version_area_id     INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    version_id          INT NOT NULL,
+    area_id             INT NOT NULL,
+    FOREIGN KEY (version_id) REFERENCES release_versions (version_id),  -- connects to release_versions:version_id
+    FOREIGN KEY (area_id)    REFERENCES areas (area_id)                 -- connects to areas:area_id
+);
 
-        
-    );
+CREATE TABLE reports (
+    report_id           INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    program_id          INT NOT NULL,
+    release_id          INT NOT NULL,
+    version_id          INT NOT NULL,
+    bug_id              INT NOT NULL,
+    report_type         VARCHAR(255) NOT NULL,
+    report_reproducible BOOLEAN NOT NULL DEFAULT FALSE,
+    FOREIGN KEY (program_id) REFERENCES programs(program_id),           -- connects to programs:program_id
+    FOREIGN KEY (release_id) REFERENCES program_releases (release_id),
+    FOREIGN KEY (version_id) REFERENCES release_versions (version_id),
+    FOREIGN KEY (bug_id)     REFERENCES bugs(bug_id)                    -- connects to bugs:bug_id
+);
+CREATE TABLE resolutions (
+    resolution_id       INT NOT NULL PRIMARY KEY,
+    report_id           INT NOT NULL,
+    area_id             INT NOT NULL,
+    resolution_status   VARCHAR(255) NOT NULL,                  -- open, closed, resolved
+    resolution_priority INT NOT NULL,                           -- 1-5
+    resolution_state    VARCHAR(255) NOT NULL,                  -- pending, fixed, irreproducible, etc.
+    resolution_version  INT NOT NULL,
+    resolution_comments VARCHAR(255),
+    resolution_defer    BOOLEAN NOT NULL DEFAULT FALSE,
+    assigned_id         INT NOT NULL,
+    resolver_id         INT NOT NULL,
+    resolver_date       DATE NOT NULL,
+    restester_id        INT NOT NULL,
+    restester_date      DATE NOT NULL,
+    FOREIGN KEY (report_id)    REFERENCES reports(report_id),   -- connected to reports:report_id
+    FOREIGN KEY (area_id)      REFERENCES areas(area_id),       -- connected to areas:area_id
+    FOREIGN KEY (assigned_id)  REFERENCES users(user_id),       -- a user is assigned
+    FOREIGN KEY (resolver_id)  REFERENCES users(user_id),       -- a user resolves
+    FOREIGN KEY (restester_id) REFERENCES users(user_id)        -- a user tests the resolution
+);
 
-    CREATE TABLE areas (
-        area_id             INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        area_title          VARCHAR(255) NOT NULL
-    );
-    CREATE TABLE reports (
-        report_id           INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        program_id          INT NOT NULL,
-        FOREIGN KEY (program_id) REFERENCES programs(program_id),        -- connects to programs:program_id
-        bug_id              INT NOT NULL,
-        FOREIGN KEY (bug_id)     REFERENCES bugs(bug_id),            -- connects to bugs:bug_id
-        report_type         VARCHAR(255) NOT NULL,
-        report_reproducible BOOLEAN NOT NULL DEFAULT FALSE
-    );
-    CREATE TABLE resolutions (
-        resolution_id       INT NOT NULL PRIMARY KEY,
-        report_id           INT NOT NULL,
-        FOREIGN KEY (report_id) REFERENCES reports(report_id),  -- connected to reports:report_id
-        area_id             INT NOT NULL,
-        FOREIGN KEY (area_id)   REFERENCES areas(area_id),      -- connected to areas:area_id
-        resolution_status   VARCHAR(255) NOT NULL,              -- open, closed, resolved
-        resolution_priority INT NOT NULL,                       -- 1-5
-        resolution_state    VARCHAR(255) NOT NULL,              -- pending, fixed, irreproducible, etc.
-        resolution_version  INT NOT NULL,
-        resolution_comments VARCHAR(255),
-        resolution_defer    BOOLEAN NOT NULL DEFAULT FALSE,  
-        
-        assigned_id         INT NOT NULL,
-        FOREIGN KEY (assigned_id) REFERENCES users(user_id),   -- a user is assigned
-        resolver_id         INT NOT NULL,
-        FOREIGN KEY (resolver_id) REFERENCES users(user_id),   -- a user resolves
-        resolver_date       DATE NOT NULL,
-        restester_id        INT NOT NULL,
-        FOREIGN KEY (restester_id) REFERENCES users(user_id), -- a user tests the resolution
-        restester_date      DATE NOT NULL
-    );
-END
 
 create procedure dropTables()
 BEGIN
@@ -86,6 +91,8 @@ BEGIN
     DROP TABLE reports;
     DROP TABLE bugs;
     DROP TABLE programs;
+    DROP TABLE program_releases;
+    DROP TABLE release_versions;
     DROP TABLE areas;
     DROP TABLE users;
 END
@@ -100,19 +107,19 @@ END
 create procedure populateTables()
 BEGIN
     -- 'users' table
-    INSERT INTO users (user_name, user_pass, user_access) VALUES ("user_Seungjun", "Seungjun_pass", 1);
-    INSERT INTO users (user_name, user_pass, user_access) VALUES ("user_Austin", "Austin_pass", 2);
-    INSERT INTO users (user_name, user_pass, user_access) VALUES ("user_Mike", "Mike_pass", 3);
-    -- 'programs' table
-    INSERT INTO programs (program_name) VALUES ("COBOL Coder v1");
-    INSERT INTO programs (program_name) VALUES ("COBOL Coder v2");
-    INSERT INTO programs (program_name) VALUES ("COBOL Coder v3");
-    INSERT INTO programs (program_name) VALUES ("COBOL Coder v4");
-    INSERT INTO programs (program_name) VALUES ("Python 3 v3.8");
-    INSERT INTO programs (program_name) VALUES ("Python 3 v3.9");
-    INSERT INTO programs (program_name) VALUES ("Python 3 v3.10");
-    INSERT INTO programs (program_name) VALUES ("Python 3 v3.11");
-    INSERT INTO programs (program_name) VALUES ("Python 3 v3.12");
+    INSERT INTO users (user_name, user_pass, user_realname, user_access) VALUES ("user_Seungjun", "Seungjun_pass", "Seungjun", 1);
+    INSERT INTO users (user_name, user_pass, user_realname, user_access) VALUES ("user_Austin", "Austin_pass", "Austin", 2);
+    INSERT INTO users (user_name, user_pass, user_realname, user_access) VALUES ("user_Mike", "Mike_pass", "Mike", 3);
+    -- -- 'programs' table
+    -- INSERT INTO programs (program_name) VALUES ("COBOL Coder v1");
+    -- INSERT INTO programs (program_name) VALUES ("COBOL Coder v2");
+    -- INSERT INTO programs (program_name) VALUES ("COBOL Coder v3");
+    -- INSERT INTO programs (program_name) VALUES ("COBOL Coder v4");
+    -- INSERT INTO programs (program_name) VALUES ("Python 3 v3.8");
+    -- INSERT INTO programs (program_name) VALUES ("Python 3 v3.9");
+    -- INSERT INTO programs (program_name) VALUES ("Python 3 v3.10");
+    -- INSERT INTO programs (program_name) VALUES ("Python 3 v3.11");
+    -- INSERT INTO programs (program_name) VALUES ("Python 3 v3.12");
     -- 'areas' table
     INSERT INTO areas (area_title) VALUES ("Back-end");
     INSERT INTO areas (area_title) VALUES ("Front-end");
@@ -125,7 +132,6 @@ END
 -- procedure calls      (call <procedure-name>)
 -- deleting procedures  (drop procedure <procedure-name>)
 drop procedure dropTables;
-drop procedure createTables;
 drop procedure populateTables;
 
 -- create procedure submitNewReport (IN program_name, report_type, report_severity, report_reproducible, report_summary, report_problem, report_suggested_fix, reporter_name, report_date, report_attachments)
