5fe43ee5008a110f1286aa9756333d59933335e9
Austin Tao <austin.t7897@gmail.com>
2024-04-19 16:52:47 -0700

    Reapply "setting up export functionality"
    This reverts commit 9961e35c57153501dfb93cca8bd16526ad36ef40.

--- script_maint.js
+++ script_maint.js
@@ -1,5 +1,19 @@
 // MAINTENANCE_PAGE
 
+var isLoggedIn = localStorage.getItem("isLoggedIn");
+
+// If user is not logged in, redirect to login page
+if (!isLoggedIn) {
+    alert("You are not logged in. Redirecting to login page...");
+  window.location.href = "Login.html";
+}
+
+var isAccessLevel = localStorage.getItem("accessLevel");
+if (isAccessLevel === 3) {
+    alert("You are not cleared to be here. Redirecting to initial menu...");
+  window.location.href = "index.html";
+}
+
 var deepLookupButton = document.getElementById("deepLookupButton");
 var redirect_employees = document.getElementById("toEmployees");
 var redirect_programs = document.getElementById("toPrograms");
@@ -36,6 +50,90 @@ if (backButton) {
     });
 }
 
+// EXPORT DB DATA
+var exportEmployeeListButton = document.getElementById("exportEmployeeListButton");
+var exportProgramAreasButton = document.getElementById("exportProgramAreasButton");
+
+if (exportEmployeeListButton) {
+    exportEmployeeListButton.addEventListener("click", function() {
+        // get file type
+        let file_type = $('#file_type_e').val();
+        console.log("beginning export of employee_list as file type:", file_type);
+
+        // get list of employees
+        const file_content = ""; // TODO: insert file content
+
+        downloadFile(file_type, file_content, "employee_list");
+    });
+}
+
+if (exportProgramAreasButton) {
+    exportProgramAreasButton.addEventListener("click", function() {
+        // get file type
+        let file_type = $('#file_type_a').val();
+        console.log("beginning export of program_areas as file type:", file_type);
+
+        // get list of program areas
+        const file_content = ""; // TODO: insert file content
+
+        downloadFile(file_type, file_content, "program_areas");
+    });
+}
+
+function downloadFile(file_type, content, src) {
+
+    if (file_type === "TXT") {
+        const link = document.createElement("a");
+        const file = new Blob([content], {type: "text/plain"});
+        
+        link.href = URL.createObjectURL(file);
+        link.download = src+".txt";
+        link.click();
+        URL.revokeObjectURL(link.href);
+
+        console.log("download of "+src+" complete");
+        return;
+    }
+
+    if (file_type === "XML") {
+
+        let parsedContent = (content) => {
+            // add parsed content, merging content where relevant
+            let xml_str = "<span></span>";
+
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(xml_str, "application/xml");
+
+            // parse failed
+            if (doc.querySelector("parseerror")) {
+                console.log("xml parse failed. no download was made.");
+                return -1;
+            }
+
+            // serialize
+            const serializer = new XMLSerializer();
+            return serializer.serializeToString(doc);
+        };
+        
+        // if parseerror happens, parsedContent will return -1 
+        if (parsedContent === -1) {return;}
+
+        const link = document.createElement("a");
+        const file = new Blob([parsedContent], {type: "text/xml"});
+        link.href = URL.createObjectURL(file);
+        link.download = src+".xml";
+        link.click();
+        URL.revokeObjectURL(link.href);
+
+        console.log("download of "+src+" complete");
+        return;
+    }
+
+    if (file_type === "JSON") {
+        console.log("not yet ready; no download has occurred");
+        return;
+    }
+}
 // MAINTENANCE LOOKUP
 
 var backButtonM = document.getElementById("backButtonM");
