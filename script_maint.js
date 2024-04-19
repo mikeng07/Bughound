5c71fb9c6ca64dcfa8924269dff4ddf067f1de5a
Mike <nguyentung2107@gmail.com>
2024-04-18 23:41:37 -0700

    update

--- script_maint.js
+++ script_maint.js
@@ -1,73 +1,73 @@
-// MAINTENANCE_PAGE
+// // MAINTENANCE_PAGE
 
-var deepLookupButton = document.getElementById("deepLookupButton");
-var redirect_employees = document.getElementById("toEmployees");
-var redirect_programs = document.getElementById("toPrograms");
-var redirect_areas = document.getElementById("toAreas");
-var backButton = document.getElementById("backButton");
+// var deepLookupButton = document.getElementById("deepLookupButton");
+// var redirect_employees = document.getElementById("toEmployees");
+// var redirect_programs = document.getElementById("toPrograms");
+// var redirect_areas = document.getElementById("toAreas");
+// var backButton = document.getElementById("backButton");
 
-if (deepLookupButton) {
-    deepLookupButton.addEventListener("click", function() {
-        window.location.href = "maintenanceLookup.html";
-    });
-}
+// if (deepLookupButton) {
+//     deepLookupButton.addEventListener("click", function() {
+//         window.location.href = "maintenanceLookup.html";
+//     });
+// }
 
-if (redirect_employees) {
-    redirect_employees.addEventListener("click", function() {
-        window.location.href = "maintenancePage/employees.html";
-    });
-}
+// if (redirect_employees) {
+//     redirect_employees.addEventListener("click", function() {
+//         window.location.href = "maintenancePage/employees.html";
+//     });
+// }
 
-if (redirect_programs) {
-    redirect_programs.addEventListener("click", function() {
-        window.location.href = "maintenancePage/programs.html";
-    });
-}
+// if (redirect_programs) {
+//     redirect_programs.addEventListener("click", function() {
+//         window.location.href = "maintenancePage/programs.html";
+//     });
+// }
 
-if (redirect_areas) {
-    redirect_areas.addEventListener("click", function() {
-        window.location.href = "maintenancePage/areas.html";
-    });
-}
+// if (redirect_areas) {
+//     redirect_areas.addEventListener("click", function() {
+//         window.location.href = "maintenancePage/areas.html";
+//     });
+// }
 
-if (backButton) {
-    backButton.addEventListener("click", function() {
-        window.location.href = "index.html";
-    });
-}
+// if (backButton) {
+//     backButton.addEventListener("click", function() {
+//         window.location.href = "index.html";
+//     });
+// }
 
-// MAINTENANCE LOOKUP
+// // MAINTENANCE LOOKUP
 
-var backButtonM = document.getElementById("backButtonM");
-if (backButtonM) {
-    backButtonM.addEventListener("click", function() {
-        window.location.href = "maintenancePage.html";
-    });
-}
+// var backButtonM = document.getElementById("backButtonM");
+// if (backButtonM) {
+//     backButtonM.addEventListener("click", function() {
+//         window.location.href = "maintenancePage.html";
+//     });
+// }
 
-var lookupButton = document.getElementById("lookupButton");
-if (lookupButton) {
-    lookupButton.addEventListener("click", function() {
+// var lookupButton = document.getElementById("lookupButton");
+// if (lookupButton) {
+//     lookupButton.addEventListener("click", function() {
 
-        let searchInput = document.getElementById("lookupType").value;
-        // $.ajax({
-        //     type: "GET",
-        //     url:  "db_connect.php",
-        //     data: {
-        //         f: "read_maint",
-        //         q: searchInput,
-        //     },
-        //     success: function(obj) {
-        //         console.log(obj);
-        //         // for (var i = 0; i < obj.length; i++) {
-        //         //     let row = "";
-        //         //     // format row
-        //         //     $('#searchResults').append(row);
-        //         // }
-        //     },
-        // });
-        // query and post results
-        $('#searchResults').html("No results found");
-    });
-}
+//         let searchInput = document.getElementById("lookupType").value;
+//         // $.ajax({
+//         //     type: "GET",
+//         //     url:  "db_connect.php",
+//         //     data: {
+//         //         f: "read_maint",
+//         //         q: searchInput,
+//         //     },
+//         //     success: function(obj) {
+//         //         console.log(obj);
+//         //         // for (var i = 0; i < obj.length; i++) {
+//         //         //     let row = "";
+//         //         //     // format row
+//         //         //     $('#searchResults').append(row);
+//         //         // }
+//         //     },
+//         // });
+//         // query and post results
+//         $('#searchResults').html("No results found");
+//     });
+// }
 
